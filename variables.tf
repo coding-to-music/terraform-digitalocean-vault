06f72b26a49266413dc5430c76ef4235dc43abb0
coding-to-music <connors.tom@gmail.com>
2022-10-01 23:27:42 +0000

    Build

--- variables.tf
+++ variables.tf
@@ -1,39 +1,30 @@
 # variable "digitalocean_token" 
 #   description = "Digitalocean auth token"
-
 # variable "domain_name" 
 #   description = "Name of the domain"
 #   default     = "example.com"
-
 # variable "droplet_image" 
 #   description = "image to use for the droplet"
 #   default     = "ubuntu-20-04-x64"
-
 # variable "project_name" 
 #   description = "Name of the project to find"
 #   default     = "Datacenter"
-
 # variable "vpc_name" 
 #   description = "Name of the VPC we are using"
 #   default     = "Datacenter"
-
 # variable "droplet_size" 
 #   description = "Size of the droplet for Vault instances"
 #   # default     = "s-1vcpu-1gb"
 #   default     = "s-1vcpu-512mb-10gb"
-
 # variable "ssh_public_key_url" 
 #   description = "URL of of the public ssh key to add to the droplet"
 #   default     = "https://github.com/brucellino.keys"
-
 # variable "instances" 
 #   description = "number of instances in the vault cluster"
 #   default     = 3
-
 # variable "username" 
 #   description = "Name of the non-root user to add"
 #   default     = "hashiuser"
-
 # variable "ssh_inbound_source_cidrs" 
 #   description = "List of CIDRs from which we will allow ssh connections on port 22"
 
